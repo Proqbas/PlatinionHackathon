b9a9574be6012c7311a386c8aa0ac4006363458b
Robin0815 <32617832+Robin0815@users.noreply.github.com>
2020-10-23 13:51:48 +0200

    API Test version 0 for tasks, runs and returns single task by calling slug of task

---
+++ BackendTest/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(64), index=True)
+    name = db.Column(db.String(64), nullable=False)
+    desc = db.Column(db.String(128), nullable=False)
---
+++ BackendTest/task.py
@@ -0,0 +1,38 @@
+import sys
+from flask import Flask, jsonify
+from models import db, Task
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///task.db"
+db.init_app(app)
+
+
+@app.route("/<slug>")
+def get_task(slug):
+    task = Task.query.filter(Task.slug == slug).first_or_404()
+    output = {
+        "name": task.name,
+        "description": task.desc,
+    }
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    # if "createdb" in sys.argv:
+    with app.app_context():
+        db.create_all()
+    print("Database created!")
+
+    # elif "seeddb" in sys.argv:
+    with app.app_context():
+        t1 = Task(slug="creategit", name="CrateGit",
+                  desc="desc1")
+        db.session.add(t1)
+        t2 = Task(slug="createui", name="CrateUI",
+                  desc="desc2")
+        db.session.add(t2)
+        db.session.commit()
+    print("Database seeded!")
+
+# else:
+    app.run(debug=True)
