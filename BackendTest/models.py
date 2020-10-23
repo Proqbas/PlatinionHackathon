9d8c40f3922a884e6a68d3c73ef3c7fdf4e5a2fe
Robin0815 <32617832+Robin0815@users.noreply.github.com>
2020-10-23 15:45:10 +0200

    Imports changed python task

--- BackendTest/models.py
+++ BackendTest/models.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     slug = db.Column(db.String(64), index=True)
     name = db.Column(db.String(64), nullable=False)
     desc = db.Column(db.String(128), nullable=True)
-    skills = db.Column(db.String(128), nullable=True)
+    #skills = db.Column(Skill, nullable=True)
 
 
 class Member(db.Model):
--- BackendTest/task.py
+++ BackendTest/task.py
@@ -1,6 +1,7 @@
 import sys
 from flask import Flask, jsonify
 from models import db, Task
+from flask_marshmallow import Marshmallow
 
 from BackendTest.models import Member
 
@@ -8,17 +9,32 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///task.db"  # change to standalone db in future
 db.init_app(app)
+ma = Marshmallow(app)
+class TaskSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('username', 'email')
+
+
+task_schema = TaskSchema()
+Tasks_schema = TaskSchema(many=True)
 
 
 # querry for the db API
 
-@app.route("/task")
-def get_slug():
-    id = request.args.get('id')
-    if id is None:
-        output = Task.query.all()
-    else:
-        output = Task.query.filterby(id).first
+# endpoint to show all users
+@app.route("/task", methods=["GET"])
+def get_task():
+    all_users = Task.query.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result.data)
+
+
+# endpoint to get user detail by id
+@app.route("/user/<id>", methods=["GET"])
+def user_detail(id):
+    user = User.query.get(id)
+    return user_schema.jsonify(user)
     """task = Task.query.filter(Task.slug == slug).first_or_404()
     output = {
         "name": task.name,
