2fb2f56ee4768b4b6ca14a3cad6d86c0c0674fe5
Matyjek <Matyjek.Jakub@platinion.com>
2020-10-23 18:10:04 +0200

    Dasboard UI

--- prjmgmt/src/components/Dashboard.jsx
+++ prjmgmt/src/components/Dashboard.jsx
@@ -26,11 +26,35 @@ class Dashboard extends Component {
           <p>
             One app to rule them all
           </p>
+          <div class="row justify-content-between">
+            <div class="col-4 element">
+              <div class="wrapperSideToSide sectionHeader">
+                <div class="left">Status:</div>
+                <div class="right projectStatusGreen">on track</div>
+              </div>
+            </div>
+            <div class="col-6 element">
+              <div class="sectionHeader">Done vs Undone</div>
+            </div>
+          </div>
+
+          <div class="clear"></div>
+
+          <div class="row justify-content-between">
+            <div class="col-4 element">
+              <div class="sectionHeader">Tasks</div>
+            </div>
+            <div class="col-6">
+              <img src="https://www.redmineup.com/cms/assets/thumbnail//32893/1200/burndown.png?token=3aab306e3a0cb67c1c9a058a375b4b46eaebb90ba18ada6e70f056ea38649b77" width="100%"/>
+            </div>
+          </div>
+
+          <div class="clear"></div>
+
           <p>
-            <Button variant="primary">Learn more</Button>
+            <Button variant="primary">Assign tasks</Button>
           </p>
         </div>
-        <div class="element">TEST ELEMENT</div>
       </div>
     );
   }
