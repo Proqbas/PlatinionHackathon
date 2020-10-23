9adfc6f88d76101ef0f81c90787c875e28c3bf77
Robin0815 <32617832+Robin0815@users.noreply.github.com>
2020-10-23 14:36:53 +0200

    Some changes

--- BackendTest/models.py
+++ BackendTest/models.py
@@ -7,4 +7,11 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     slug = db.Column(db.String(64), index=True)
     name = db.Column(db.String(64), nullable=False)
-    desc = db.Column(db.String(128), nullable=False)
+    desc = db.Column(db.String(128), nullable=True)
+    skills = db.Column(db.String(128), nullable=True)
+
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(64), index=True)
+    name = db.Column(db.String(64), nullable=False)
--- BackendTest/task.py
+++ BackendTest/task.py
@@ -2,6 +2,8 @@ import sys
 from flask import Flask, jsonify
 from models import db, Task
 
+from BackendTest.models import Member
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///task.db"
 db.init_app(app)
@@ -13,6 +15,7 @@ def get_task(slug):
     output = {
         "name": task.name,
         "description": task.desc,
+        "skills": task.skills,
     }
     return jsonify(output)
 
@@ -26,13 +29,14 @@ if __name__ == "__main__":
     # elif "seeddb" in sys.argv:
     with app.app_context():
         t1 = Task(slug="creategit", name="CrateGit",
-                  desc="desc1")
+                  desc="desc1", skills="nan")
         db.session.add(t1)
         t2 = Task(slug="createui", name="CrateUI",
-                  desc="desc2")
+                  desc="desc2", skills="nan")
         db.session.add(t2)
+        m1 = Member(slug="", name= )
         db.session.commit()
     print("Database seeded!")
 
-# else:
+    # else:
     app.run(debug=True)
